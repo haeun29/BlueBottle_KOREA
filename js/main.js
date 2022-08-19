7bd12819eb8da52db5fa597d91be2ec2c1a3fb3d
han <105402303+haeun29@users.noreply.github.com>
2022-08-19 15:38:05 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper("#pcBanner", {
     direction: "vertical",
     slidesPerView: 1,
     spaceBetween: 30,
@@ -9,13 +9,30 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-  var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-
+  var swiper = new Swiper("#mBanner", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    }
   });
 
 
+
+
+
 AOS.init();
+
+
+$(function(){
+
+	$(".m-toggle-btn").click(function(){
+		$("header nav").toggleClass("on");
+	});
+
+
+ 
+
+}); // $
 
